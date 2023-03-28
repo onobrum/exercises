ca606976d7dde47a91e8189c44334c5d4139026e
Gabriela Ono Brum <112426740+onobrum@users.noreply.github.com>
2023-03-27 21:52:13 -0300

    Update advanced-functions.js

--- advanced-functions.js
+++ advanced-functions.js
@@ -1,38 +1,44 @@
-//Solve these problems:
+Solve these problems:
 
-//#1 Create a one line function that adds two parameters
+#1 Create a one line function that adds two parameters
+              //const sum = (a, b) => a + b
 
-//answer: const sum = (a, b) => a + b
 
-
-//Closure: What does the last line return?
+Closure: What does the last line return?
+  
 const addTo = x => y => x + y
 var addToTen = addTo(10)
-addToTen(3) //answer: 13
+addToTen(3) 
+              //13
 
 
-//Currying: What does the last line return?
+Currying: What does the last line return?
+  
 const sum = (a, b) => a + b
 const curriedSum = (a) => (b) => a + b
-curriedSum(30)(1) //answer: 31
+curriedSum(30)(1) 
+              //31
 
 
 
-//Currying: What does the last line return?
+Currying: What does the last line return?
+  
 const sum = (a, b) => a + b
 const curriedSum = (a) => (b) => a + b
 const add5 = curriedSum(5)
-add5(12) //answer: 17
+add5(12) 
+               //17
 
 
-//Composing: What does the last line return?
+Composing: What does the last line return?
+  
 const compose = (f, g) => (a) => f(g(a));
 const add1 = (num) => num + 1;
 const add5 = (num) => num + 5;
-compose(add1, add5)(10) //answer: 16
+compose(add1, add5)(10) 
+               //16
 
 
-//What are the two elements of a pure function?
-
-//answer: Deterministic and no side effects.
+What are the two elements of a pure function?
+              // Deterministic and no side effects.
 
