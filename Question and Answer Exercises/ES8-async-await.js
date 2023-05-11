23c7f1d4e35ea26e2df6a81009a8dbb0dd0f585f
Gabriela Ono Brum <112426740+onobrum@users.noreply.github.com>
2023-05-11 18:22:47 -0300

    Update ES8-async-await.js

--- Question and Answer Exercises/ES8-async-await.js
+++ Question and Answer Exercises/ES8-async-await.js
@@ -1,13 +1,13 @@
-Solve the below problem:
+//Solve the below problem:
 
-#1) Convert the below promise into async await
+//#1) Convert the below promise into async await
 
-fetch("https://jsonplaceholder.typicode.com/users/")
-  .then((response) => response.json())
-  .then(console.log);
+//fetch("https://jsonplaceholder.typicode.com/users/")
+  //.then((response) => response.json())
+  //.then(console.log);
 
-                //async function fetchStarship() {
-                  //const response = await fetch("https://jsonplaceholder.typicode.com/users");
-                  //const data = await response.json();
-                  //console.log(data);
-                //}
+                async function fetchStarship() {
+                  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+                  const data = await response.json();
+                  console.log(data);
+                }
