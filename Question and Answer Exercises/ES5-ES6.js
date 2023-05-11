24123b3dfd716cfb6e3a6a41cae4b2b29040a5f5
Gabriela Ono Brum <112426740+onobrum@users.noreply.github.com>
2023-05-11 18:18:39 -0300

    Update ES5-ES6.js

--- Question and Answer Exercises/ES5-ES6.js
+++ Question and Answer Exercises/ES5-ES6.js
@@ -1,91 +1,100 @@
-1- change everything below to the newer Javascript!
+//1- change everything below to the newer Javascript!
 
-let + const
-var a = 'test';  
-var b = true;   
-var c = 789;   
-a = 'test2';
-                            //let a = 'test'; 
-                            //const b = true;
-                            //const c = 789;
-                            //a = 'test2';
+//let + const
+//var a = 'test';  
+//var b = true;   
+//var c = 789;   
+//a = 'test2';
 
-
-2- Destructuring
-
-var person = {
-    firstName : "John",
-    lastName  : "Doe",
-    age       : 50,
-    eyeColor  : "blue"
-};
-
-var firstName = person.firstName;
-var lastName = person.lastName;
-var age = person.age;
-var eyeColor = person.eyeColor;
-                            //const person = {
-                            //firstName : "John",
-                            //lastName  : "Doe",
-                            //age       : 50,
-                            //eyeColor  : "blue"
-                            //};
-
-                            //const { firstName, lastName, age, eyeColor } = person;
+                            let a = 'test'; 
+                            const b = true;
+                            const c = 789;
+                            a = 'test2';
 
 
-3- Object properties
 
-var a = 'test';
-var b = true;
-var c = 789;
+//2- Destructuring
 
-var okObj = {
-  a: a,
-  b: b,
-  c: c
-};
-                            //const a = 'test';
-                            //const b = true;
-                            //const c = 789;
+//var person = {
+    //firstName : "John",
+    //lastName  : "Doe",
+    //age       : 50,
+    //eyeColor  : "blue"
+//};
 
-                            //const okObj = {a, b, c};
+//var firstName = person.firstName;
+//var lastName = person.lastName;
+//var age = person.age;
+//var eyeColor = person.eyeColor;
 
+                            const person = {
+                            firstName : "John",
+                            lastName  : "Doe",
+                            age       : 50,
+                            eyeColor  : "blue"
+                            };
 
-4- Template strings
-
-var message = "Hello " + firstName + " have I met you before? I think we met in " + city + " last summer no???";
-                            //const message = `Hello ${firstName} have I met you before? I think we met in ${city}. last summer no???`;
-
-
-5- default arguments
-
-default age to 10;
-function isValidAge(age) {      
-    return age
-}
-                            //const isValidAge = (age = 10) => age;const isValidAge = (age = 10) => age;
+                            const { firstName, lastName, age, eyeColor } = person;
 
 
-6- Symbol
 
-Create a symbol: "This is my first Symbol"
-                            //const sym = Symbol('This is my first Symbol');
+//3- Object properties
 
-7- Arrow functions
+//var a = 'test';
+//var b = true;
+//var c = 789;
 
-    function whereAmI(username, location) {
-        if (username && location) {
-    if (username && location) {
-        return "I am not lost";
-    } else {
-        return "I am totally lost!";
-    }
-}
-                        //const whereAmI = (username, location) => {
-                        //if (username && location) {
-                        //return "I am not lost";
-                        //} else {
-                        //return "I am totally lost!";
-                        //}
-                        //}
+//var okObj = {
+  //a: a,
+  //b: b,
+  //c: c
+//};
+
+                            const a = 'test';
+                            const b = true;
+                            const c = 789;
+
+                            const okObj = {a, b, c};
+
+
+
+//4- Template strings
+
+//var message = "Hello " + firstName + " have I met you before? I think we met in " + city + " last summer no???";
+                            
+                            const message = `Hello ${firstName} have I met you before? I think we met in ${city}. last summer no???`;
+
+
+//5- default arguments
+
+//default age to 10;
+//function isValidAge(age) {      
+    //return age
+//}
+                            const isValidAge = (age = 10) => age;const isValidAge = (age = 10) => age;
+
+
+//6- Symbol
+
+//Create a symbol: "This is my first Symbol"
+                            
+                            const sym = Symbol('This is my first Symbol');
+
+
+//7- Arrow functions
+
+    //function whereAmI(username, location) {
+        //if (username && location) {
+    //if (username && location) {
+        //return "I am not lost";
+    //} else {
+        //return "I am totally lost!";
+    //}
+//}
+                        const whereAmI = (username, location) => {
+                        if (username && location) {
+                        return "I am not lost";
+                        } else {
+                        return "I am totally lost!";
+                        }
+                        }
