e47d4a9614c28420119087e25b25c02ef0cdb2b2
Gabriela Ono Brum <112426740+onobrum@users.noreply.github.com>
2023-05-11 22:43:13 -0300

    Create ES8-async-await.js

---
+++ ES8-async-await.js
@@ -0,0 +1,13 @@
+//Solve the below problem:
+
+//#1) Convert the below promise into async await
+
+//fetch("https://jsonplaceholder.typicode.com/users/")
+  //.then((response) => response.json())
+  //.then(console.log);
+
+                async function fetchStarship() {
+                  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+                  const data = await response.json();
+                  console.log(data);
+                }
